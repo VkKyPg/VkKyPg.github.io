5237531a425bbe16183a68d237bbedc795d98720
Vikki Pang <1234kwan1@gmail.com>
2016-07-23 22:09:01 -0400

    changed button

--- javascript/index.js
+++ javascript/index.js
@@ -2,6 +2,7 @@ alert("In js file1");
 $(document).ready(function(){
   $(".Class_Expand").hide();
   $(".Year_Expand").click(function(){
+    alert("Just clicked button");
     if( $(this).find('input[type="button"]').val() == '+')
     {
       $(this).find('input[type="button"]').val("-");
