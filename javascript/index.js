cf2715cc25e4219116df3b1032e14f0e5f049e3a
Vikki Pang <1234kwan1@gmail.com>
2016-07-23 20:15:32 -0400

    tryjs

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,4 @@
+  windows.alert("In js file");
 $(document).ready(function(){
   windows.alert("This is inside js");
-  $("Freshmen Year").click(function(){
-    windows.alert("this is inside click");
-    $("Fresh Fall Classes").toggle();
-  });
 });
