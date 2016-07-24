00fa63b90ec3d4532229616e3a2a0f90f0934ad5
Vikki Pang <1234kwan1@gmail.com>
2016-07-23 21:59:51 -0400

    changed +/-

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,12 @@
 alert("In js file1");
 $(document).ready(function(){
   $(".Class_expand").hide();
+  if( $(this).find('input[type="button"]').val() == '+')
+  {
+    $(this).find('input[type="button"]').val("-");
+  }
+  else
+  {
+    $(this).find('input[type="button"]').val("+");
+  }
 });
