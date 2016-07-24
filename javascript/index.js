82c5a846d35caaddc4dac48355ed475eeb30970e
Vikki Pang <1234kwan1@gmail.com>
2016-07-23 22:11:02 -0400

    changed button

--- javascript/index.js
+++ javascript/index.js
@@ -5,10 +5,12 @@ $(document).ready(function(){
     alert("Just clicked button");
     if( $(this).find('input[type="button"]').val() == '+')
     {
+      alert("Checked if it was plus");
       $(this).find('input[type="button"]').val("-");
     }
     else
     {
+      alert("check if it was subtract");
       $(this).find('input[type="button"]').val("+");
     }
   });
