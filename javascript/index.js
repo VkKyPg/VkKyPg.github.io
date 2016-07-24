dcb9a7fb266b8cab056a6e5aec4d6a4d2f149f83
Vikki Pang <1234kwan1@gmail.com>
2016-07-23 22:25:50 -0400

    changed button

--- javascript/index.js
+++ javascript/index.js
@@ -3,17 +3,16 @@ $(document).ready(function(){
   $(".Class_Expand").hide();
   $(".Year_Expand").click(function(){
     alert("Just clicked button");
-    alert($(this).find('input[type="button"]').val());
-    if( $(this).find('input[type="button"]').val() == "+")
+    if( $(this).val() == "+")
     {
       alert("Checked if it was plus");
-      $(this).find('input[type="button"]').val("-");
+      $(this).val("-");
       alert("Changed to negative");
     }
     else
     {
       alert("check if it was subtract");
-      $(this).find('input[type="button"]').val("+");
+      $(this).val("+");
       alert("Changed to positive");
     }
   });
