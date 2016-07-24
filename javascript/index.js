aacd2ca547e57350e3f8d671d89ea7aaf65b7c64
Vikki Pang <1234kwan1@gmail.com>
2016-07-23 21:26:23 -0400

    class

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,5 @@
-alert("In js file");
+alert("In js file1");
 $(document).ready(function(){
-  $("Class expand").hide()
-  alert("This is inside js");
+  $(".Class expand").hide();
+  alert("This is inside js2");
 });
