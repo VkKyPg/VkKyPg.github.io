d18c0ce6d3afced5b5dad642201ef09167ea3bb6
Vikki Pang <1234kwan1@gmail.com>
2016-07-23 20:11:03 -0400

    tryjs

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
+  windows.alert("This is inside js");
   $("Freshmen Year").click(function(){
+    windows.alert("this is inside click");
     $("Fresh Fall Classes").toggle();
   });
 });
