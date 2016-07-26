112b194d3deaf33dae9fd550581ff801a868c856
Vikki Pang <1234kwan1@gmail.com>
2016-07-25 22:55:26 -0400

    toggle

--- javascript/index.js
+++ javascript/index.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
 
     alert("before thing toggled");
     Specific_class = $(event.target).closest("h2").find(".Class_Expand");
+    $("body").on("click",".Year_Expand", Specific_class.toggle());
     alert("after thing was toggled");
 
     if( $(this).val() == "+")
