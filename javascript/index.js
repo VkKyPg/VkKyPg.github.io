30244342cd0ea14297210bfd5ddf886414c08c54
Vikki Pang <1234kwan1@gmail.com>
2016-07-26 22:29:46 -0400

    toggle

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
   $(".Class_Expand").hide();
   $(".Year_Expand").click(function(){
-    $(this).parent().next(".Class_Expand").("h3").toggle();
+    $(this).parent().next(".Class_Expand").toggle();
     if( $(this).val() == "+")
     {
       $(this).val("-");
+      $(this).parent().next(".Class_Expand").("h3").toggle();
     }
     else
     {
