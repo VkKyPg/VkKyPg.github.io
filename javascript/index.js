e461a8e5ef6eabf84afc5ce62d8bc379be75ae47
Vikki Pang <1234kwan1@gmail.com>
2016-07-26 22:28:08 -0400

    toggle

--- javascript/index.js
+++ javascript/index.js
@@ -1,25 +1,18 @@
-alert("In js file1");
 $(document).ready(function(){
   $(".Class_Expand").hide();
   $(".Year_Expand").click(function(){
-    alert("Just clicked button");
-
-    alert("before thing toggled");
-    $(this).parent().next(".Class_Expand").toggle();
-    alert("after thing was toggled");
-
+    $(this).parent().next(".Class_Expand").("h3").toggle();
     if( $(this).val() == "+")
     {
-      alert("Checked if it was plus");
       $(this).val("-");
-      alert("Changed to negative");
     }
     else
     {
-      alert("check if it was subtract");
       $(this).val("+");
-      alert("Changed to positive");
     }
-
   });
+
+
+
+
 });
